fe83decdf54b3350e2f02e666d89982f37a396f4
Priyansh27u <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-08-19 11:25:37 +0530

    firstchange

--- JavaControlFlows/Divisible.java
+++ JavaControlFlows/Divisible.java
@@ -1,4 +1,15 @@
 package JavaControlFlows;
 
+import java.util.Scanner;
+
 public class Divisible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        if(num1%5==0){
+            System.out.println(num1+" is Divisible by 5");
+        } else {
+            System.out.println("Not Divisible by 5");
+        }
+    }
 }
