642bf30d8221c6bf8899d028459385aabd30c2eb
Priyanshu Varshney <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-09-09 11:39:54 +0530

    Create CarRental.java

---
+++ Constructors/CarRental.java
@@ -0,0 +1,24 @@
+public class CarRental {
+    private String customerName;
+    private String carModel;
+    private int rentalDays;
+    private double dailyRate;
+
+    public CarRental() {
+        this.customerName = "";
+        this.carModel = "";
+        this.rentalDays = 0;
+        this.dailyRate = 1000.0;
+    }
+
+    public CarRental(String customerName, String carModel, int rentalDays, double dailyRate) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+        this.dailyRate = dailyRate;
+    }
+
+    public double calculateTotalCost() {
+        return rentalDays * dailyRate;
+    }
+}
