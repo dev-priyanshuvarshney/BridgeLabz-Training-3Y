fdf1d56382979ff4ad8cd0be04054ce6bb3b549e
Priyanshu Varshney <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-09-09 11:46:53 +0530

    Create BankAccount.java

---
+++ Constructors/Instance vs. Class Variables and Methods/BankAccount.java
@@ -0,0 +1,55 @@
+class BankAccount {
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid withdrawal.");
+        }
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: " + balance);
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, String accountHolder, double balance, double interestRate) {
+        super(accountNumber, accountHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void displaySavingsDetails() {
+        System.out.println("Account Number (public): " + accountNumber);
+        System.out.println("Account Holder (protected): " + accountHolder);
+        System.out.println("Balance (via getter): " + getBalance());
+        System.out.println("Interest Rate: " + interestRate + "%");
+    }
+}
