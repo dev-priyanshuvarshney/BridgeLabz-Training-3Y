1a59f45954ddb6696115836b3cfd8cc58c49c102
Priyanshu Varshney <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-09-09 14:40:17 +0530

    Create SubstringComparison.java

---
+++ Strings/SubstringComparison.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SubstringComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        System.out.print("Enter start index: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end index: ");
+        int end = sc.nextInt();
+
+        String customSubstring = createSubstring(text, start, end);
+        String builtInSubstring = text.substring(start, end);
+
+        System.out.println("Substring using charAt(): " + customSubstring);
+        System.out.println("Substring using substring(): " + builtInSubstring);
+
+        boolean comparisonResult = compareStrings(customSubstring, builtInSubstring);
+
+        if (comparisonResult) {
+            System.out.println("Both substrings are equal.");
+        } else {
+            System.out.println("Substrings are different.");
+        }
+
+        sc.close();
+    }
+
+    public static String createSubstring(String str, int start, int end) {
+        String result = "";
+        for (int i = start; i < end; i++) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
+
+    public static boolean compareStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
