b6f137e2d044691435ac4cd9e9371d3a22f81c48
Priyanshu Varshney <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-09-09 11:45:36 +0530

    Create Book.java

---
+++ Constructors/Instance vs. Class Variables and Methods/Book.java
@@ -0,0 +1,41 @@
+class Book {
+    public String ISBN;
+    protected String title;
+    private String author;
+
+    public Book(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void displayDetails() {
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+class EBook extends Book {
+    private double fileSize;
+
+    public EBook(String ISBN, String title, String author, double fileSize) {
+        super(ISBN, title, author);
+        this.fileSize = fileSize;
+    }
+
+    public void displayEBookDetails() {
+        System.out.println("ISBN (public): " + ISBN);
+        System.out.println("Title (protected): " + title);
+        System.out.println("Author (via getter): " + getAuthor());
+        System.out.println("File Size: " + fileSize + " MB");
+    }
+}
