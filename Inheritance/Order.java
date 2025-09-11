0a43dd61c8395e7e61b4434d408636e2ec429868
Priyanshu Varshney <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-09-11 19:35:12 +0530

    Create Order.java

---
+++ Inheritance/Order.java
@@ -0,0 +1,44 @@
+class Order {
+    String orderId;
+    String orderDate;
+
+    Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    String getOrderStatus() {
+        return "Order placed on " + orderDate;
+    }
+}
+
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    ShippedOrder(String orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order shipped. Tracking Number: " + trackingNumber;
+    }
+}
+
+class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    DeliveredOrder(String orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order delivered on " + deliveryDate;
+    }
+}
+
+public class OnlineRetailOrderManagement {
+    public static void main(String[] a
